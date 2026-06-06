3cfd4402c017cb438cae11ea40191676e45f426a
laillinurfajri <laillinurfajrikaderisasi@gmail.com>
2026-06-06 21:31:22 +0700

    menambahkan isi modul moodiary

---
+++ tugas-09/manager/mood_tracker.py
@@ -0,0 +1,7 @@
+from tkinter import messagebox
+
+def klik_mood():
+    messagebox.showinfo(
+        "Mood Tracker",
+        "Fitur Mood Tracker masih dalam pengembangan."
+    )
---
+++ tugas-09/manager/quote.py
@@ -0,0 +1,7 @@
+from tkinter import messagebox
+
+def klik_quote():
+    messagebox.showinfo(
+        "Quote of The Day",
+        "Tetap semangat, setiap langkah kecil adalah kemajuan."
+    )
